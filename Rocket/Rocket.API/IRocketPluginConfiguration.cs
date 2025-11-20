0d304ccf9d47dfc12b86add52a340cda64387b39
bedforthdanivo-dev <bedforthdanivo@gmail.com>
2025-11-20 08:40:38 +0200

    Update IRocketPluginConfiguration.cs
    yetti

--- Rocket/Rocket.API/IRocketPluginConfiguration.cs
+++ Rocket/Rocket.API/IRocketPluginConfiguration.cs
@@ -2,5 +2,9 @@
 {
     public interface IRocketPluginConfiguration : IDefaultable
     {
+    <?xml version="1.0" encoding="utf-8"?>
+<Config xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Key>ce9ffad9-1ca5-4b52-8d52-73ab43382e01</Key>
+</Config>
     }
 }
